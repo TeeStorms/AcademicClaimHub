f95b4e179e96e9a3418b65d5f4ad0a48a5caf913
TeeStorms <zothando.mathunyane@gmail.com>
2025-08-27 15:40:53 +0200

    Applied dark theme styling, navigation header, and reporting enhancements

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,21 +7,26 @@ namespace AcademicClaimHub
         public MainWindow()
         {
             InitializeComponent();
+            // Default page
+            GoToLecturer(this, new RoutedEventArgs());
         }
 
         private void GoToLecturer(object sender, RoutedEventArgs e)
         {
             MainContent.Content = new Views.LecturerDashboard();
+            txtCurrentPage.Text = "Lecturer Dashboard";
         }
 
         private void GoToCoordinator(object sender, RoutedEventArgs e)
         {
             MainContent.Content = new Views.ProgrammeCoordinatorDashboard();
+            txtCurrentPage.Text = "Programme Coordinator Dashboard";
         }
 
         private void GoToManager(object sender, RoutedEventArgs e)
         {
             MainContent.Content = new Views.AcademicManagerDashboard();
+            txtCurrentPage.Text = "Academic Manager Dashboard";
         }
     }
 }
